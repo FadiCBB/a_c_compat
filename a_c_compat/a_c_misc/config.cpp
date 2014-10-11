fbbe835aca2691046bb3739873931dba2feeee38
Fadi <sdanon10110@gmail.com>
2014-10-11 14:52:51 -0500

    Added by CSE in 0.7.0

--- a_c_compat/a_c_misc/config.cpp
+++ a_c_compat/a_c_misc/config.cpp
@@ -18,10 +18,10 @@ class Extended_PostInit_EventHandlers
 		clientInit = "0 = [] spawn compile preProcessFileLineNumbers '\a_c_misc\cse_action_menu\misc_drop_ruck.sqf'";
 	};
 	// Note: CSE includes joining and leaving groups however there is no way to take control of a group.
-	class a_c_group_management
+/*	class a_c_group_management
 	{
 		clientInit = "0 = [] spawn compile preProcessFileLineNumbers '\a_c_misc\cse_action_menu\misc_group_management.sqf'";
-	};
+	};*/
 	class a_c_weapon_on_back
 	{
 		clientInit = "0 = [] spawn compile preProcessFileLineNumbers '\a_c_misc\cse_action_menu\misc_weapon_on_back.sqf'";
